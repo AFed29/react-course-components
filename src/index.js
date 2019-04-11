eb31667b2701b103a6b92c03ed03b90c79b32c55
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2019-04-11 16:20:03 +0100

    adding passing component through props

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 4:45PM" 
-        commentText="Nice one!" 
-        avatar={faker.image.avatar()} 
-      />
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Today at 2:00PM" 
-        commentText="What a great blog post" 
-        avatar={faker.image.avatar()} 
-      />
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Yesterday at 3:59PM" 
-        commentText="I want some more!!" 
-        avatar={faker.image.avatar()} 
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:45PM" 
+          commentText="Nice one!" 
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Today at 2:00PM" 
+          commentText="What a great blog post" 
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Yesterday at 3:59PM" 
+          commentText="I want some more!!" 
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
     </div>
   );
 };
